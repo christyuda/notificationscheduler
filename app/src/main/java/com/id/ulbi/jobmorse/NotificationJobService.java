09cc3e64705acd7f888cebc9a536c4c2058756d8
hapis20 <53987991+christyuda@users.noreply.github.com>
2022-12-20 10:23:48 +0700

    pemrog job scheduler

---
+++ app/src/main/java/com/id/ulbi/jobmorse/NotificationJobService.java
@@ -0,0 +1,41 @@
+package com.id.ulbi.jobmorse;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+public class NotificationJobService extends JobService {
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+
+        //Set up the notification content intent to launch the app when clicked
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (this, 0, new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setContentTitle(getString(R.string.job_service))
+                .setContentText(getString(R.string.job_running))
+                .setContentIntent(contentPendingIntent)
+                .setSmallIcon(R.drawable.ic_running)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+
+        manager.notify(0, builder.build());
+
+        return false;
+    }
+
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return true;
+    }
+}
